e0f0468f36f7a21303e58c042eeb3107415aad17
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-01 12:51:16 +0300

    sdsd

--- Lesson6/Task1/webpack.config.js
+++ Lesson6/Task1/webpack.config.js
@@ -6,19 +6,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /.(jpg|png)$/,
@@ -27,6 +16,7 @@ module.exports = {
             loader: "url-loader",
             options: {
               limit: 8192,
+              name: "[name].[ext]",
               outputPath: "images",
             },
           },
