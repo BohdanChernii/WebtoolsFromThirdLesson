3f331deb7c6a6416132287b76179fbdeed92ead2
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-01 12:31:18 +0300

    vvxv

--- Lesson6/Task1/webpack.config.js
+++ Lesson6/Task1/webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
           {
             loader: "url-loader",
             options: {
-              limit: 8000,
+              limit: 8192,
               outputPath: "images",
             },
           },
